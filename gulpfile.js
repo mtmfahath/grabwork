fdbe62b9518cbb21cb538eb66f6b800c5baae8dd
Mohamed Fahath <mtmfahath@gmail.com>
2018-07-19 01:24:47 +0530

    After NPM Installed

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
+
+
+gulp.task('default',function(){
+  console.log("hooray - you created a Gulp taks");
+});
+
+gulp.task('html',function(){
+  console.log("Imagine something useful being done to your html");
+});
+
+gulp.task('styles',function(){
+  return gulp.src('./app/assets/styles/styles.css')
+  .pipe(postcss([cssvars,nested,autoprefixer]))
+  .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch',function(){
+
+  watch('./app/index.html', function(){
+    gulp.start('html');
+  });
+
+  watch('./app/assets/styles/**/*.css', function(){
+    gulp.start('styles');
+  });
+
+});
